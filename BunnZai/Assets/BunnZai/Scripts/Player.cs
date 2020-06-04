111f0a57d573903483e5809ed5745cff4d491003
MATOO <fagoldob@gmail.com>
2020-06-04 21:40:03 +0200

    Started basic implementation of new Player class

---
+++ BunnZai/Assets/BunnZai/Scripts/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    PlayerBasicMovement mBasicMovement;
+    PlayerAdvancedMovement mAdvancedMovement;
+    PlayerCameraController mCameraController;
+    PlayerCombatController mCombatController;
+
+    void Awake()
+    {
+        mBasicMovement = GetComponent<PlayerBasicMovement>();
+        mAdvancedMovement = GetComponent<PlayerAdvancedMovement>();
+        mCameraController = GetComponent<PlayerCameraController>();
+        mCombatController = GetComponent<PlayerCombatController>();
+    }
+
+    void FixedUpdate()
+    {
+        //e.g. mBasicMovement.Jump();
+    }
+}
