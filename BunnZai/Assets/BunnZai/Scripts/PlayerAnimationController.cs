7a6538731fa1b00e60805080bb6004f8fa15742f
MATOO <fagoldob@gmail.com>
2020-07-01 16:19:05 +0200

    Connected Animator to Variables using AnimationController

--- BunnZai/Assets/BunnZai/Scripts/PlayerAnimationController.cs
+++ BunnZai/Assets/BunnZai/Scripts/PlayerAnimationController.cs
@@ -5,9 +5,24 @@ using UnityEngine;
 public class PlayerAnimationController : MonoBehaviour
 {
     Player mPlayer;
+    Animator mAnimator;
 
     public void Awake()
     {
         mPlayer = GetComponent<Player>();
+        mAnimator = GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        //mAnimator.SetFloat("name", value);
+        mAnimator.SetFloat("velocityOverall", mPlayer.mRigidBody.velocity.magnitude);
+        mAnimator.SetFloat("velocityHorizontal", new Vector3(mPlayer.mRigidBody.velocity.x, 0, mPlayer.mRigidBody.velocity.z).magnitude);
+        mAnimator.SetFloat("velocityVertical", mPlayer.mRigidBody.velocity.y);
+        mAnimator.SetFloat("forwardAxisDelta", mPlayer.mForwardAxisDelta);
+        mAnimator.SetFloat("sidewaysAxisDelta", mPlayer.mSidewaysAxisDelta);
+        mAnimator.SetBool("isGrounded", mPlayer.mIsGrounded);
+        mAnimator.SetBool("aerialJumpUsed", mPlayer.mAerialJumpUsed);
+        mAnimator.SetBool("jumpPressed", Input.GetButtonDown("Jump"));
     }
 }
